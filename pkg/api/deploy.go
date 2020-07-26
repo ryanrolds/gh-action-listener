12898f4cba4bd0a16bac6685c7a0c8faaf9ca8cd
Ryan Olds <ryanrolds@gmail.com>
2020-07-26 16:12:29 -0700

    Additional login

--- pkg/api/deploy.go
+++ pkg/api/deploy.go
@@ -6,7 +6,26 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	ParamRepo = "repo"
+	ParamTag  = "tag"
+)
+
 func (a *API) DeployHandler(w http.ResponseWriter, r *http.Request) {
-	logrus.Info("deploy handler called")
+	repo, ok := r.URL.Query()[ParamRepo]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("missing repo query param"))
+		return
+	}
+
+	tag, ok := r.URL.Query()[ParamTag]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("missing tag query param"))
+		return
+	}
+
+	logrus.Infof("deploy handler called %s %s", repo, tag)
 	w.WriteHeader(http.StatusOK)
 }
